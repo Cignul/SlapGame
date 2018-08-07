eb776301f550ee83ce8ebe02dbe7552f7f4fb264
Jackson Gordon <gordon.jacksonh@gmail.com>
2018-08-07 15:27:41 -0600

    trying to link jquery stuff

--- game.js
+++ game.js
@@ -5,74 +5,75 @@ let resetButton = $('#reset-button');
 let gameButtons = $('#slap-button,#kick-button,#smash-button,#apple-button,#heart-button,#strawberry-button');
 //attacks
 function slap() {
-  health -= 1;
-  update();
+  health -= 10;
   // return alert(health)
+  reset();
 }
 function kick() {
   health -= 5;
-  update();
+  reset();
   // return alert(health)
 }
 function smash() {
   health -= 10;
-  update();
+  reset();
   // return alert(health)
 }
 //healing stuff
 function heart() {
-  health += 10;
-  update();
+  health += 100;
+  reset();
 }
 function apple() {
-  health += 100;
-  update();
+  health += 10;
+  reset();
 
 }
 function strawberry() {
   health += 20;
-  update();
+  reset();
 
 }
-function update(attack) {
+function reset(attack) {
   if (health < 100) {
     health = 100;
   }
-  if (health >= 0) {
-    //gameOver
+  if (health == 0) {
+    gameOver()
   }
-  if (attack == true) {
+  if (attack === true) {
     hits++;
-    updateView();
+  };
+  updateView();
+
+
+  function gameOver() {
+    health = 0;
+    resetButton.removeClass("invisible")
+    gameButtons.addClass("invisible")
+
+    //
+    //
+  }
+  function updateView() {
+    $("health").text(health)
+    $("hits").text(hits)
+    if (health > 75) {
+      //first photo
+      $('img[src]').attr("src", "starfox.jpg")
+
+    } if (health < 75 && health > 25) {
+      //second photo
+      $('img[src]').attr("src", "starfoxdmged.jpg")
+    }
+    else {
+      //most damaged photo
+    }
   }
 
+  slap
+  kick
+  smash
+  heart
+  apple
 }
-function gameOver() {
-  health = 0;
-  resetButton.removeClass("invisible")
-  gameButtons.addClass("invisible")
-
-  //
-  //
-}
-function updateView() {
-  $("health").text(health)
-  $("hits").text(hits)
-  if (health > 75) {
-    //first phoot
-    $('img[src]').attr("src", "starfox.jpg")
-
-  } if (health < 75 && health > 25) {
-    //second photo
-    $('img[src]').attr("src", "starfoxdmged.jpg")
-  }
-  else {
-    //most damaged photo
-  }
-}
-
-slap
-kick
-smash
-heart
-apple
