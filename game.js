c4a37dc974d2817aadad93f1184fadd8d581da5b
Jackson Gordon <gordon.jacksonh@gmail.com>
2018-08-07 14:58:35 -0600

    trying to add jquery stuff

--- game.js
+++ game.js
@@ -1,28 +1,37 @@
+
 var health = 100;
 var hits = 0;
+let resetButton = $('#reset-button');
+let gameButtons = $('#slap-button,#kick-button,#smash-button,#apple-button,#heart-button,#strawberry-button');
 //attacks
 function slap() {
   health -= 1;
+  update();
   // return alert(health)
 }
 function kick() {
   health -= 5;
+  update();
   // return alert(health)
 }
 function smash() {
   health -= 10;
+  update();
   // return alert(health)
 }
 //healing stuff
 function heart() {
   health += 10;
+  update();
 }
 function apple() {
   health += 100;
+  update();
 
 }
 function strawberry() {
   health += 20;
+  update();
 
 }
 function update(attack) {
@@ -30,19 +39,36 @@ function update(attack) {
     health = 100;
   }
   if (health >= 0) {
-    //endgame
+    //gameOver
   }
   if (attack == true) {
-    hits++
+    hits++;
+    updateView();
   }
+
 }
 function gameOver() {
-  health = 100;
+  health = 0;
+  resetButton.removeClass("invisible")
+  gameButtons.addClass("invisible")
+
   //
   //
 }
 function updateView() {
-  //jquery stuff
+  $("health").text(health)
+  $("hits").text(hits)
+  if (health > 75) {
+    //first phoot
+    $('img[src]').attr("src", "starfox.jpg")
+
+  } if (health < 75 && health > 25) {
+    //second photo
+    $('img[src]').attr("src", "starfoxdmged.jpg")
+  }
+  else {
+    //most damaged photo
+  }
 }
 
 slap
